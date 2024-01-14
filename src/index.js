cd083ce1bccfcc38f22e0fc0fa4a8526f7862238
marx peng <35778015+ixmu@users.noreply.github.com>
2024-01-14 11:21:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "${workername}.${username}.workers.dev/": "https://registry-1.docker.io",
 };
 
 function routeByHosts(host) {
