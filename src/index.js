69d3c7b72390e8e96988c31a1f01d84369bc878a
HWUA Co.,Ltd <13664256@qq.com>
2024-06-07 20:18:03 +0800

    Update index.js
    add oracleoaec

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.oracleoaec.com": "https://registry-1.docker.io",
+  "quay.oracleoaec.com": "https://quay.io",
+  "gcr.oracleoaec.com": "https://gcr.io",
+  "k8s-gcr.oracleoaec.com": "https://k8s.gcr.io",
+  "k8s.oracleoaec.com": "https://registry.k8s.io",
+  "ghcr.oracleoaec.com": "https://ghcr.io",
+  "cloudsmith.oracleoaec.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
