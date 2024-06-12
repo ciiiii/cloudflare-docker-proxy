1177b7f30c39aee0fbb32caef71d6be62b4ea17b
spencerswagger <51152244+spencerswagger@users.noreply.github.com>
2024-06-12 13:15:50 +0800

    修改域名映射

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.usewheel.online": "https://registry-1.docker.io",
+  "quay.docker.usewheel.online": "https://quay.io",
+  "gcr.docker.usewheel.online": "https://gcr.io",
+  "k8s-gcr.docker.usewheel.online": "https://k8s.gcr.io",
+  "k8s.docker.usewheel.online": "https://registry.k8s.io",
+  "ghcr.docker.usewheel.online": "https://ghcr.io",
+  "cloudsmith.docker.usewheel.online": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
