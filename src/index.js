ed0ef097c5260aba19407e9b0c95e2ee633681ef
周子健 <183465355@qq.com>
2024-06-13 22:02:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.zecdn.top": "https://registry-1.docker.io",
+  "quay.zecdn.top": "https://quay.io",
+  "gcr.zecdn.top": "https://gcr.io",
+  "k8s-gcr.zecdn.top": "https://k8s.gcr.io",
+  "k8s.zecdn.top": "https://registry.k8s.io",
+  "ghcr.zecdn.top": "https://ghcr.io",
+  "cloudsmith.zecdn.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
