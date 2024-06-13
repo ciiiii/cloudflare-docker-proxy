cf4e411a29490167aad1579507f831d99eff6ca5
BingoXuan <xuanzebing.bingo@gmail.com>
2024-06-13 17:26:36 +0800

    use private route

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker-mirror.yourstream.cloud": "https://registry-1.docker.io",
+  // "quay.libcuda.so": "https://quay.io",
+  // "gcr.libcuda.so": "https://gcr.io",
+  // "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
+  // "k8s.libcuda.so": "https://registry.k8s.io",
+  // "ghcr.libcuda.so": "https://ghcr.io",
+  // "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
