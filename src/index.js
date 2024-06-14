42ee4db257dd2bb65852e0a6e6d5cb5a00038af1
绅士喵 <meow.i5.br@gmail.com>
2024-06-14 13:06:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,7 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "${workername}.${username}.workers.dev/": dockerHub,
 };
 
 function routeByHosts(host) {
