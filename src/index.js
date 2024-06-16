42829a80123044812270643a2ccbae7894d7ddbe
cdryzun <yangzun@treesir.pub>
2024-06-16 22:13:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "dockerhub.cpinnov.run": dockerHub,
+  "quay.cpinnov.run": "https://quay.io",
+  "gcr.cpinnov.run": "https://gcr.io",
+  "k8s-gcr.cpinnov.run": "https://k8s.gcr.io",
+  "k8s.cpinnov.run": "https://registry.k8s.io",
+  "ghcr.cpinnov.run": "https://ghcr.io",
+  "cloudsmith.cpinnov.run": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cpinnov.run": dockerHub,
 };
 
 function routeByHosts(host) {
