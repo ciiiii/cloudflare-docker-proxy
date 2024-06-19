38d91f048401ac812f3a955e6092c05692473307
jerryzhang721 <122506888+jerryzhang721@users.noreply.github.com>
2024-06-19 21:27:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.jerrynote.com": dockerHub,
+  // "quay.jerrynote.com": "https://quay.io",
+  //"gcr.jerrynote.com": "https://gcr.io",
+  //"k8s-gcr.jerrynote.com": "https://k8s.gcr.io",
+  //"k8s.jerrynote.com": "https://registry.k8s.io",
+  //"ghcr.jerrynote.com": "https://ghcr.io",
+  //"cloudsmith.jerrynote.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.jerrynote.com": dockerHub,
 };
 
 function routeByHosts(host) {
