a810f05200891f17fc977d747848defb5654ffc6
AlexMa2011 <72970078+AlexMa2011@users.noreply.github.com>
2024-06-23 01:05:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.alexma.top": dockerHub,
+  "quay.alexma.top": "https://quay.io",
+  "gcr.alexma.top": "https://gcr.io",
+  "k8s-gcr.alexma.top": "https://k8s.gcr.io",
+  "k8s.alexma.top": "https://registry.k8s.io",
+  "ghcr.alexma.top": "https://ghcr.io",
+  "cloudsmith.alexma.top": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
