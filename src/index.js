e25436f885571b70a7ebe8bf08c49cdc19300276
鹏程 <zhangpengcheng820@163.com>
2024-07-09 17:38:35 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.pengcheng.team": dockerHub,
+  "quay.pengcheng.team": "https://quay.io",
+  "gcr.pengcheng.team": "https://gcr.io",
+  "k8s-gcr.pengcheng.team": "https://k8s.gcr.io",
+  "k8s.pengcheng.team": "https://registry.k8s.io",
+  "ghcr.pengcheng.team": "https://ghcr.io",
+  "cloudsmith.pengcheng.team": "https://docker.cloudsmith.io",
+  "ecr.pengcheng.team": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.pengcheng.team": dockerHub,
 };
 
 function routeByHosts(host) {
