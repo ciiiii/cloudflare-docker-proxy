1c4deb12236d25a541963e48a2f2b475c30dfe2d
LPF <lpf929@126.com>
2024-07-09 16:21:14 +0800

    fix

--- README.md
+++ README.md
@@ -30,11 +30,11 @@
    - add more records and modify the config as you need
    ```javascript
    const routes = {
-     "docker.libcuda.so": "https://registry-1.docker.io",
-     "quay.libcuda.so": "https://quay.io",
-     "gcr.libcuda.so": "https://k8s.gcr.io",
-     "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-     "ghcr.libcuda.so": "https://ghcr.io",
+     "docker.nvue.dev": "https://registry-1.docker.io",
+     "quay.nvue.dev": "https://quay.io",
+     "gcr.nvue.dev": "https://k8s.gcr.io",
+     "k8s-gcr.nvue.dev": "https://k8s.gcr.io",
+     "ghcr.nvue.dev": "https://ghcr.io",
    };
    ```
 
--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.nvue.dev": dockerHub,
+  "quay.nvue.dev": "https://quay.io",
+  "gcr.nvue.dev": "https://gcr.io",
+  "k8s-gcr.nvue.dev": "https://k8s.gcr.io",
+  "k8s.nvue.dev": "https://registry.k8s.io",
+  "ghcr.nvue.dev": "https://ghcr.io",
+  "cloudsmith.nvue.dev": "https://docker.cloudsmith.io",
+  "ecr.nvue.dev": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.nvue.dev": dockerHub,
 };
 
 function routeByHosts(host) {
