9fc24bdf01e56098ef911410ae627bc36c8c9b00
Weijie <weijie.luan@outlook.com>
2024-07-16 16:55:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.yeeauth.com": dockerHub,
+  "quay.yeeauth.com": "https://quay.io",
+  "gcr.yeeauth.com": "https://gcr.io",
+  "k8s-gcr.yeeauth.com": "https://k8s.gcr.io",
+  "k8s.yeeauth.com": "https://registry.k8s.io",
+  "ghcr.yeeauth.com": "https://ghcr.io",
+  "cloudsmith.yeeauth.com": "https://docker.cloudsmith.io",
+  "ecr.yeeauth.com": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
