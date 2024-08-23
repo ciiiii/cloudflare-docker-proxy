be7dbc150a58579d8389cc18e92f9e35f100ca0c
wbshi168 <154323674+wbshi168@users.noreply.github.com>
2024-08-23 11:18:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.wbshi.top": dockerHub,
+  "quay.wbshi.top": "https://quay.io",
+  "gcr.wbshi.top": "https://gcr.io",
+  "k8s-gcr.wbshi.top": "https://k8s.gcr.io",
+  "k8s.wbshi.top": "https://registry.k8s.io",
+  "ghcr.wbshi.top": "https://ghcr.io",
+  "cloudsmith.wbshi.top": "https://docker.cloudsmith.io",
+  "ecr.wbshi.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.wbshi.top": dockerHub,
 };
 
 function routeByHosts(host) {
