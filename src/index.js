afb0c3294081640a2a7749b58bf5f8831d0e139f
wankun <wankun@apache.org>
2024-09-12 22:18:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker2.58658.site": dockerHub,
+  "quay.58658.site": "https://quay.io",
+  "gcr.58658.site": "https://gcr.io",
+  "k8s-gcr.58658.site": "https://k8s.gcr.io",
+  "k8s.58658.site": "https://registry.k8s.io",
+  "ghcr.58658.site": "https://ghcr.io",
+  "cloudsmith.58658.site": "https://docker.cloudsmith.io",
+  "ecr.58658.site": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.58658.site": dockerHub,
 };
 
 function routeByHosts(host) {
