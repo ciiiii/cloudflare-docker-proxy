c514538aa3ade93368a068e9ce39bbc56cec7dca
Chace <22810924+ChaceYang@users.noreply.github.com>
2024-09-29 14:06:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.alow.store": dockerHub,
+  "quay.alow.store": "https://quay.io",
+  "gcr.alow.store": "https://gcr.io",
+  "k8s-gcr.alow.store": "https://k8s.gcr.io",
+  "k8s.alow.store": "https://registry.k8s.io",
+  "ghcr.alow.store": "https://ghcr.io",
+  "cloudsmith.alow.store": "https://docker.cloudsmith.io",
+  "ecr.alow.store": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.alow.store": dockerHub,
 };
 
 function routeByHosts(host) {
