1a572dff2d97cacb6fefc8132d9552264a95cdb9
fanyun <745442170@qq.com>
2024-11-11 11:04:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.jackproxy.fun": dockerHub,
+  "v4.jackproxy.fun": dockerHub,
   "quay.jackproxy.fun": "https://quay.io",
   "gcr.jackproxy.fun": "https://gcr.io",
   "k8s-gcr.jackproxy.fun": "https://k8s.gcr.io",
