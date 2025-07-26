2b0daa280b4e96b59e8e900a8fe66c8d6b18c83e
loficore <164591859+loficore@users.noreply.github.com>
2025-07-26 12:50:17 +0800

    Update index.js
    Initial changes step1

--- src/index.js
+++ src/index.js
@@ -6,19 +6,15 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.flyforfree.cc": "https://registry-1.docker.io",
+  "quay.flyforfree.cc": "https://quay.io",
+  "gcr.flyforfree.cc": "https://gcr.io",
+  "k8s-gcr.flyforfree.cc": "https://k8s.gcr.io",
+  "k8s.flyforfree.cc": "https://registry.k8s.io",
+  "ghcr.flyforfree.cc": "https://ghcr.io",
+  "cloudsmith.flyforfree.cc": "https://docker.cloudsmith.io",
 };
+
 
 function routeByHosts(host) {
   if (host in routes) {
